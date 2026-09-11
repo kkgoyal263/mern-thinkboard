f74c36a52d9b58ddba9ae2759043a153930fe647
kkgoyal263 <Ryomensukuna24k@gmail.com>
2026-09-11 20:14:27 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 import express from "express"; // if we want to use this add "type": "module" in package.json
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import {connectDB} from "./config/db.js";
@@ -12,11 +13,16 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-}));
+if(process.env.NODE_ENV !== "production"){
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }
+  ));
+}
+
 //middleware - used for rate limiting
 app.use(express.json());
 app.use(rateLimiter);
@@ -24,13 +30,17 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
   });
-})
+});
 
-
-//shygTJLirlk5IvNL
-
-//mongodb+srv://ryomensukuna24k_db_user:shygTJLirlk5IvNL@cluster0.bxybqph.mongodb.net/?appName=Cluster0
